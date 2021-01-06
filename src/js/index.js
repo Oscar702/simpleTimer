7d390985526a6b19e6acec2ca59462d920f1c481
Oscar702 <FunesLV@aol.com>
2021-01-06 19:59:54 +0000

    making it

--- src/js/index.js
+++ src/js/index.js
@@ -11,5 +11,19 @@ import "../styles/index.scss";
 //import your own components
 import { Home } from "./component/home.js";
 
+function Counter(props) {
+	return (
+		<div className="mainCounter">
+			<div className="clock">
+				<i className="far fa-clock" />
+			</div>
+			<div className="four">8</div>
+			<div className="three" />
+			<div className="two" />
+			<div className="one" />
+		</div>
+	);
+}
+
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Counter />, document.querySelector("#app"));
